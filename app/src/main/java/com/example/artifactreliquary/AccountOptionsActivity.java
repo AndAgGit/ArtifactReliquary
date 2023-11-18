b5bb367f6bd84189ef29f33cda69f379ddac422e
afonc <andaguilar@csumb.edu>
2023-11-17 20:14:42 -0800

    Well I accidentally did this on master again BUT added log out buttons

--- app/src/main/java/com/example/artifactreliquary/AccountOptionsActivity.java
+++ app/src/main/java/com/example/artifactreliquary/AccountOptionsActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.artifactreliquary.databinding.ActivityAccountOptionsBinding;
 import com.example.artifactreliquary.databinding.ActivityLoginOptionBinding;
 
+import java.util.List;
+
 public class AccountOptionsActivity extends AppCompatActivity {
     ActivityAccountOptionsBinding binding;
+
+    Button logOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,22 @@ public class AccountOptionsActivity extends AppCompatActivity {
 
         binding = ActivityAccountOptionsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        logOutButton = binding.logOutButton;
+        logOutButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                List<User> userList = MainActivity.userDAO.getActiveUser();
+                User toUpdate = userList.get(0);
+                toUpdate.setActive(false);
+                MainActivity.userDAO.update(toUpdate);
+
+                Intent intent = MainActivity.getIntent(getApplicationContext());
+                startActivity(intent);
+
+                return false;
+            }
+        });
     }
 
     public static Intent getIntent(Context context){
--- app/src/main/java/com/example/artifactreliquary/AdminOptionsActivity.java
+++ app/src/main/java/com/example/artifactreliquary/AdminOptionsActivity.java
@@ -3,14 +3,20 @@ package com.example.artifactreliquary;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.artifactreliquary.databinding.ActivityAccountOptionsBinding;
 import com.example.artifactreliquary.databinding.ActivityAdminOptionsBinding;
 
+import java.util.List;
+
 public class AdminOptionsActivity extends AppCompatActivity {
     ActivityAdminOptionsBinding binding;
+
+    Button logOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,22 @@ public class AdminOptionsActivity extends AppCompatActivity {
 
         binding = ActivityAdminOptionsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        logOutButton = binding.logOutButton;
+        logOutButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                List<User> userList = MainActivity.userDAO.getActiveUser();
+                User toUpdate = userList.get(0);
+                toUpdate.setActive(false);
+                MainActivity.userDAO.update(toUpdate);
+
+                Intent intent = MainActivity.getIntent(getApplicationContext());
+                startActivity(intent);
+
+                return false;
+            }
+        });
     }
 
     public static Intent getIntent(Context context){
