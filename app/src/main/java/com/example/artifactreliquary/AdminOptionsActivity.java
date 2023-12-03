cda970478861322090c5f05f1c62d140ffe3637f
afonc <andaguilar@csumb.edu>
2023-12-03 12:21:04 -0800

    tiny wording change

--- app/src/main/java/com/example/artifactreliquary/AdminOptionsActivity.java
+++ app/src/main/java/com/example/artifactreliquary/AdminOptionsActivity.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class AdminOptionsActivity extends AppCompatActivity {
     ActivityAdminOptionsBinding binding;
 
-    Button logOutButton;
+    Button logOutButton, userButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
